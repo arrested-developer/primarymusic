07328262a1155a76e32f42c77bf9b21b919376b4
sima-qian <29804933+sima-qian@users.noreply.github.com>
2018-09-12 16:00:54 +0100

    refactor drum sound & score functionality

--- public/assets/scripts/dom.js
+++ public/assets/scripts/dom.js
@@ -1,10 +1,7 @@
 /* eslint-disable */
 
 var scoreDisplay = document.getElementById("score");
-var drum1 = document.getElementById("drum1");
-var drum10 = document.getElementById("drum10");
-var drum100 = document.getElementById("drum100");
-var drum1000 = document.getElementById("drum1000");
+var drums = document.querySelectorAll(".drum");
 
 var currentScore = score();
 
@@ -14,18 +11,10 @@ function hitDrum(num, drum) {
   playSound(drum);
 }
 
-drum1.addEventListener("click", function() {
-  hitDrum(1, "drum1");
-});
-
-drum10.addEventListener("click", function() {
-  hitDrum(10, "drum10");
-});
-
-drum100.addEventListener("click", function() {
-  hitDrum(100, "drum100");
-});
-
-drum1000.addEventListener("click", function() {
-  hitDrum(1000, "drum1000");
+drums.forEach(function(drum) {
+  var drumId = drum.id;
+  var drumScore = Number(drum.id.slice(4));
+  drum.addEventListener("click", function() {
+    hitDrum(drumScore, drumId);
+  });
 });
