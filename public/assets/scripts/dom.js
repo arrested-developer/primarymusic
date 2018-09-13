cf75416954b7c05af6c31ad0fbbe6c265d54200c
martingaston <squidman@gmail.com>
2018-09-13 16:09:37 +0100

    Refactor dom.js and remove numdrum.scss as it's not being used Relates #25

--- public/assets/scripts/dom.js
+++ public/assets/scripts/dom.js
@@ -1,29 +1,29 @@
 /* eslint-disable */
 
+// declare page elements
 var scoreDisplay = document.getElementById("score");
 var drums = document.querySelectorAll(".drum");
 
+// instantiate player score
 var currentScore = score();
 
+// reset button
+document.getElementById("reset").addEventListener("click", function() {
+  document.querySelector("#score > h1").textContent = currentScore.reset();
+});
+
+// hitDrum is a wrapper function which calls individual gameplay elements as each drum is hit
 function hitDrum(num, drum) {
-  currentScore.add(num);
-  var s = document.createElement("h1");
-  var o = scoreDisplay.querySelector("h1");
-  s.textContent = currentScore.get();
-  scoreDisplay.replaceChild(s, o);
+  document.querySelector("#score > h1").textContent = currentScore.add(num);
   playSound(drum);
   document.getElementById(drum).classList.toggle(drum + "--clicked");
 }
 
+// go through drums (has class .drum) and attach eventListeners to call hitDrum();
 drums.forEach(function(drum) {
   var drumId = drum.id;
   var drumScore = Number(drum.id.slice(4));
   drum.addEventListener("click", function() {
     hitDrum(drumScore, drumId);
   });
-});
-
-document.getElementById("reset").addEventListener("click", function() {
-  currentScore.reset();
-  document.querySelector("#score > h1").textContent = "0";
 });
