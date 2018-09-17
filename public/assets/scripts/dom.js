d3dc86f5a104a89524a53b163e1d62a8382d5626
BryMon <monika.flisowska@gmail.com>
2018-09-17 14:59:47 +0100

    add touchend event listener, relates #43 #45

--- public/assets/scripts/dom.js
+++ public/assets/scripts/dom.js
@@ -23,7 +23,7 @@ function hitDrum(num, drum) {
 drums.forEach(function(drum) {
   var drumId = drum.id;
   var drumScore = Number(drum.id.slice(4));
-  drum.addEventListener("click", function() {
+  drum.addEventListener("touchend", function() {
     hitDrum(drumScore, drumId);
   });
 });
