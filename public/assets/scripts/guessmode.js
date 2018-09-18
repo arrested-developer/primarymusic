9cb45f65d11e9c167bbb0a6abf5382b2b6fa8bd2
Michael Watts <hello@michaelwatts.co>
2018-09-18 13:20:28 +0100

    add guess the number mode

---
+++ public/assets/scripts/guessmode.js
@@ -0,0 +1,18 @@
+const startGuessMode = () => {
+  //instantiate two scores
+  const scores = {
+    playerOne: score(),
+    playerTwo: score()
+  };
+
+  const getTaps = score => {
+    return score + 3;
+  };
+
+  // start a new round
+  const playRound = score => {
+    const taps = getTaps(score);
+    console.log(`you have ${taps} taps`);
+    currentScore.reset();
+  };
+};
