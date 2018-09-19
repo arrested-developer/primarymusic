a0cb8ff1f7fdb324473134966ec9f8761e649d6c
sima-qian <29804933+sima-qian@users.noreply.github.com>
2018-09-19 10:42:37 +0100

    some linting

--- public/manup.js
+++ public/manup.js
@@ -1,12 +1,12 @@
 //this is the object we build for an ajax call to get the content of the manifest
 //it's a JSON, so we can parse it like a man-child
 //verion 0.7
-var manUpObject;
+let manUpObject;
 
 //data objects
-var tagArray = [],
+let tagArray = [],
   linkArray = [];
-var validMetaValues = [
+const validMetaValues = [
   { name: "mobile-web-app-capable", manifestName: "display" },
   { name: "apple-mobile-web-app-capable", manifestName: "display" },
   { name: "apple-mobile-web-app-title", manifestName: "short_name" },
@@ -33,7 +33,7 @@ var validMetaValues = [
     imageSize: "310x310"
   }
 ];
-var validLinkValues = [
+const validLinkValues = [
   { name: "apple-touch-icon", manifestName: "icons", imageSize: "152x152" },
   { name: "apple-touch-icon", manifestName: "icons", imageSize: "120x120" },
   { name: "apple-touch-icon", manifestName: "icons", imageSize: "76x76" },
@@ -46,13 +46,13 @@ var validLinkValues = [
 ];
 
 //these next two classes are building the mixed data, pulling out the values we need based on the valid values array
-var generateFullMetaData = function() {
-  for (var i = 0; i < validMetaValues.length; i++) {
+const generateFullMetaData = function() {
+  for (let i = 0; i < validMetaValues.length; i++) {
     if (manUpObject[validMetaValues[i].manifestName]) {
       if (validMetaValues[i].manifestName == "icons") {
         //here we need to loop through each of the images to see if they match
-        var imageArray = manUpObject.icons;
-        for (var j = 0; j < imageArray.length; j++) {
+        const imageArray = manUpObject.icons;
+        for (let j = 0; j < imageArray.length; j++) {
           if (imageArray[j].sizes == validMetaValues[i].imageSize) {
             //problem is in here, need to asign proper value
             validMetaValues[i].content = imageArray[j].src;
@@ -75,13 +75,13 @@ var generateFullMetaData = function() {
   return validMetaValues;
 };
 
-var generateFullLinkData = function() {
-  for (var i = 0; i < validLinkValues.length; i++) {
+const generateFullLinkData = function() {
+  for (let i = 0; i < validLinkValues.length; i++) {
     if (manUpObject[validLinkValues[i].manifestName]) {
       if (validLinkValues[i].manifestName == "icons") {
         //here we need to loop through each of the images to see if they match
-        var imageArray = manUpObject.icons;
-        for (var j = 0; j < imageArray.length; j++) {
+        const imageArray = manUpObject.icons;
+        for (let j = 0; j < imageArray.length; j++) {
           if (imageArray[j].sizes == validLinkValues[i].imageSize) {
             //problem is in here, need to asign proper value
             validLinkValues[i].content = imageArray[j].src;
@@ -100,22 +100,22 @@ var generateFullLinkData = function() {
 
 //These are the 2 functions that build out the tags
 //TODO: make it loop once instead of tx
-var generateMetaArray = function() {
-  var tempMetaArray = generateFullMetaData();
-  var headTarget = document.getElementsByTagName("head")[0];
-  for (var i = 0; i < tempMetaArray.length; i++) {
-    var metaTag = document.createElement("meta");
+const generateMetaArray = function() {
+  const tempMetaArray = generateFullMetaData();
+  const headTarget = document.getElementsByTagName("head")[0];
+  for (let i = 0; i < tempMetaArray.length; i++) {
+    const metaTag = document.createElement("meta");
     metaTag.name = tempMetaArray[i].name;
     metaTag.content = tempMetaArray[i].content;
     headTarget.appendChild(metaTag);
   }
 };
 
-var generateLinkArray = function() {
-  var tempLinkArray = generateFullLinkData();
-  var headTarget = document.getElementsByTagName("head")[0];
-  for (var i = 0; i < tempLinkArray.length; i++) {
-    var linkTag = document.createElement("link");
+const generateLinkArray = function() {
+  const tempLinkArray = generateFullLinkData();
+  const headTarget = document.getElementsByTagName("head")[0];
+  for (let i = 0; i < tempLinkArray.length; i++) {
+    const linkTag = document.createElement("link");
     linkTag.setAttribute("rel", tempLinkArray[i].name);
     linkTag.setAttribute("sizes", tempLinkArray[i].imageSize);
     linkTag.setAttribute("href", tempLinkArray[i].content);
@@ -124,7 +124,7 @@ var generateLinkArray = function() {
   }
 };
 //these functions makes the ajax calls and assigns to manUp object
-var generateObj = function(ajaxString) {
+const generateObj = function(ajaxString) {
   //for testing
   //document.getElementById("output").innerHTML = ajaxString;
   //gen object
@@ -133,12 +133,12 @@ var generateObj = function(ajaxString) {
   generateMetaArray();
 };
 
-var makeAjax = function(url) {
+const makeAjax = function(url) {
   if (!window.XMLHttpRequest) return;
-  var fullURL;
-  var pat = /^https?:\/\//i;
+  let fullURL;
+  const pat = /^https?:\/\//i;
   pat.test(url) ? (fulURL = url) : (fullURL = window.location.hostname + url);
-  var ajax = new XMLHttpRequest();
+  const ajax = new XMLHttpRequest();
   ajax.onreadystatechange = function() {
     if (ajax.readyState == 4 && ajax.status == 200)
       generateObj(ajax.responseText);
@@ -147,14 +147,14 @@ var makeAjax = function(url) {
   ajax.send();
 };
 
-var collectManifestObj = function() {
-  var links = document.getElementsByTagName("link");
-  for (var i = 0; i < links.length; i++) {
+const collectManifestObj = function() {
+  const links = document.getElementsByTagName("link");
+  for (let i = 0; i < links.length; i++) {
     if (links[i].rel && links[i].rel == "manifest") makeAjax(links[i].href);
   }
 };
 
-var testForManifest = (function() {
+const testForManifest = (function() {
   //i'm not sure what to do here.  I am starchly against browser sniffing and there is no test (yet)
   //I think I'm jsut going to build out the tags on every page until there is a way to test it, so far
   //it looks like the manifest will override the tags
--- public/pwabuilder-sw.js
+++ public/pwabuilder-sw.js
@@ -1,42 +1,36 @@
 //This is the "Offline page" service worker
 
 //Install stage sets up the offline page in the cache and opens a new cache
-self.addEventListener("install", function(event) {
-  var offlinePage = new Request("offline.html");
+self.addEventListener("install", (event) => {
+  const offlinePage = new Request("offline.html");
   event.waitUntil(
-    fetch(offlinePage).then(function(response) {
-      return caches.open("pwabuilder-offline").then(function(cache) {
+    fetch(offlinePage).then((response) => caches.open("pwabuilder-offline").then((cache) => {
         console.log(
           "[PWA Builder] Cached offline page during Install" + response.url
         );
         return cache.put(offlinePage, response);
-      });
-    })
+      }))
   );
 });
 
 //If any fetch fails, it will show the offline page.
 //Maybe this should be limited to HTML documents?
-self.addEventListener("fetch", function(event) {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    fetch(event.request).catch(function(error) {
+    fetch(event.request).catch((error) => {
       console.error(
         "[PWA Builder] Network request Failed. Serving offline page " + error
       );
-      return caches.open("pwabuilder-offline").then(function(cache) {
-        return cache.match("offline.html");
-      });
+      return caches.open("pwabuilder-offline").then((cache) => cache.match("offline.html"));
     })
   );
 });
 
 //This is a event that can be fired from your page to tell the SW to update the offline page
-self.addEventListener("refreshOffline", function(response) {
-  return caches.open("pwabuilder-offline").then(function(cache) {
+self.addEventListener("refreshOffline", (response) => caches.open("pwabuilder-offline").then((cache) => {
     console.log(
       "[PWA Builder] Offline page updated from refreshOffline event: " +
         response.url
     );
     return cache.put(offlinePage, response);
-  });
-});
+  }));
