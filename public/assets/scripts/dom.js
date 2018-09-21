b561845f1c5b5cd7275b84f3ac25552a92da19c3
Michael Watts <hello@michaelwatts.co>
2018-09-21 12:23:49 +0100

    set up service worker

--- public/assets/scripts/dom.js
+++ public/assets/scripts/dom.js
@@ -6,3 +6,9 @@ document.querySelector("#timed").addEventListener("click", () => timedMode());
 document
   .querySelector("#twoPlayer")
   .addEventListener("click", () => alert("Coming Soon!"));
+
+// register service worker
+if ("serviceWorker" in navigator) {
+  // register service worker
+  navigator.serviceWorker.register("/service-worker.js");
+}
